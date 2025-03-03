c4341a9ec1d7be6afef25dc704ed490bfa50060a
coding.emojii <75626033+AlessandroBonomo28@users.noreply.github.com>
2025-03-03 23:09:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ Xvfb :1 -screen 0 1024x768x16 &
 # BRAINROT UPLOADER 
 #sleep 1
 #echo launching 2 backends and brainrot backend invoker - brainrot uploader
-#cd /home/username/openbrainrot/Backend
-#source /home/username/openbrainrot/env/bin/activate
+cd /home/username/Openbrainrot/Backend
+source /home/username/Openbrainrot/env/bin/activate
 
 # BRAINROT UPLOADER 
 #python /home/username/openbrainrot/Backend/brainrot.py & 
 #python /home/username/openbrainrot/Backend/brainrot_uploader.py
 
 # REDDIT MEME UPLOADER
-python /home/username/openbrainrot/Backend/reddit_meme_uploader.py
+python /home/username/Openbrainrot/Backend/reddit_meme_uploader.py
